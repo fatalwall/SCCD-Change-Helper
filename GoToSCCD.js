3ce6f131b05fc2e2fbc2d506766c385903676be2
fatalwall <pvarney@vshed.us>
2017-09-11 12:50:27 -0400

    Corrected default value for url to match options

--- GoToSCCD.js
+++ GoToSCCD.js
@@ -2,18 +2,14 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	OpenSCCD();
 });
 
-
-
-
 function OpenSCCD() {
 	chrome.storage.sync.get(
 		['sccdurl'], 
 		function(items) {
-			var sccdurl = items.sccdurl || 'https://sccd';
+			var sccdurl = items.sccdurl || 'https://sccd/maximo';
 			chrome.tabs.create({url:sccdurl});
 		}
 	);
 
 }
-
 
