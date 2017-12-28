4598a5c4711e7bb03aa0adea79bd9540cd32d5a6
Peter Varney <pvarney@vshed.us>
2017-12-28 15:44:58 -0500

    Added options for new ticket buttons

--- src/options/options.js
+++ src/options/options.js
@@ -4,7 +4,7 @@
 
 function loadOptions() {
 	chrome.storage.sync.get(
-		['sccdurl', 'showAS', 'reminderTime', 'reminderUnit', 'reminderEnabled', 'email'], 
+		['sccdurl', 'showAS', 'reminderTime', 'reminderUnit', 'reminderEnabled', 'email', 'newTicketChange', 'newTicketProblem', 'newTicketIncident', 'newTicketServiceRequest'], 
 		function(items) {
 			document.getElementById('sccdurl').value = items.sccdurl || 'https://sccd/maximo';
 			document.getElementById('showAS').value = items.showAS || 'FREE';
@@ -16,6 +16,30 @@ function loadOptions() {
 				document.getElementById('reminderEnabled').checked = items.reminderEnabled;
 			}
 			document.getElementById('email').value = items.email || '';
+			
+			if (items.newTicketChange === undefined){
+				document.getElementById('newTicketChange').checked = false;
+			} else {
+				document.getElementById('newTicketChange').checked = items.newTicketChange;
+			}
+			
+			if (items.newTicketProblem === undefined){
+				document.getElementById('newTicketProblem').checked = false;
+			} else {
+				document.getElementById('newTicketProblem').checked = items.newTicketProblem;
+			}
+			
+			if (items.newTicketIncident === undefined){
+				document.getElementById('newTicketIncident').checked = true;
+			} else {
+				document.getElementById('newTicketIncident').checked = items.newTicketIncident;
+			}
+			
+			if (items.newTicketServiceRequest === undefined){
+				document.getElementById('newTicketServiceRequest').checked = true;
+			} else {
+				document.getElementById('newTicketServiceRequest').checked = items.newTicketServiceRequest;
+			}
 		}
 	);
 }
@@ -27,6 +51,10 @@ function storeOptions() {
 	var reminderUnit = document.getElementById('reminderUnit').value;
 	var reminderEnabled = document.getElementById('reminderEnabled').checked;
 	var email = document.getElementById('email').value;
+	var newTicketChange = document.getElementById('newTicketChange').checked;
+	var newTicketProblem = document.getElementById('newTicketProblem').checked;
+	var newTicketIncident = document.getElementById('newTicketIncident').checked;
+	var newTicketServiceRequest = document.getElementById('newTicketServiceRequest').checked;
 	chrome.storage.sync.set(
 		{
 		'sccdurl': sccdurl,
@@ -34,7 +62,11 @@ function storeOptions() {
 		'reminderTime': reminderTime,
 		'reminderUnit': reminderUnit,
 		'reminderEnabled': reminderEnabled,
-		'email': email
+		'email': email,
+		'newTicketChange': newTicketChange,
+		'newTicketProblem': newTicketProblem,
+		'newTicketIncident': newTicketIncident,
+		'newTicketServiceRequest': newTicketServiceRequest
 		}, 
 		function() {
 			console.log('Options Saved');
@@ -52,4 +84,9 @@ window.onload = function() {
   document.getElementById('reminderEnabled').onchange = storeOptions;
   document.getElementById('email').onchange = storeOptions;
 
+  //New Ticket Buttons
+  document.getElementById('newTicketChange').onchange = storeOptions;
+  document.getElementById('newTicketProblem').onchange = storeOptions;
+  document.getElementById('newTicketIncident').onchange = storeOptions;
+  document.getElementById('newTicketServiceRequest').onchange = storeOptions;
 }
